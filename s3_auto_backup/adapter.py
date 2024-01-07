5077ee372fca2d12f42bf3ef04e4b66ce6efb90a
unknown <merz.sebastian95@gmail.com>
2024-01-07 19:05:16 +0100

    testing pgp encryption

--- s3_auto_backup/adapter.py
+++ s3_auto_backup/adapter.py
@@ -6,16 +6,40 @@ import subprocess
 from datetime import datetime, timezone
 import os
 import croniter
+import shutil
 
 hass_options = json.load(open('/data/options.json'))
 log = lambda value: subprocess.run(f'echo "{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")} | {str(value)}"', shell=True) if hass_options['logging'] else lambda: None
 s3 = boto3.client('s3', aws_access_key_id = hass_options['access_key_id'], aws_secret_access_key = hass_options['secret_access_key'])
 
+def encrypt_with_pgp():
+    shutil.copyfile(hass_options["pgp_public_key_path"], "public.key")
+    # subprocess.run("gpg --import public.key", shell=True)
+    # res = subprocess.Popen(["gpg", "--import", "public.key"], stdout=subprocess.PIPE)
+    #res = subprocess.run("gpg --import public.key", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+    # log("TESTING: " + str(res.communicate()))
+    subprocess.run("gpg --import public.key", shell=True, stdout=subprocess.PIPE)
+
+    test = subprocess.run("gpg --list-keys", shell=True, stdout=subprocess.PIPE)
+    key_name = str([i for i in test.stdout.decode('utf-8').strip().split('\n') if i.startswith('uid')][0].split('] ')[1])
+    log("key_name: " + key_name)
+
+    commands = "gpg --output "+hass_options["pgp_public_key_path"]+".gpg --encrypt --trust-model always --recipient " + key_name + "  " + hass_options["pgp_public_key_path"]
+    res = subprocess.run(commands, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+    log("TESTING2: " + str(res))
+
 def create_and_upload_backup():
     log('Creating backup...')
     res = requests.post('http://supervisor/backups/new/full', json = { "password": hass_options['backup_password'] } if 'backup_password' in hass_options.keys() else None, headers = { 'Authorization': 'Bearer ' + os.environ.get('SUPERVISOR_TOKEN') })
     if res.status_code == 200:
-        log('Backup created successfully! Start uploading it to S3...')
+        log('Backup created successfully!')
+
+        # Encrypt if needed
+        if 'pgp_public_key' in hass_options.keys():
+            log('Start encrypting backup...')
+            log('Backup encrypting successfully!')
+
+        log('Start uploading it to S3...')
         try:
             backup_path = f'/backup/{res.json()["data"]["slug"]}.tar'
             s3.upload_file(backup_path, hass_options['bucket_name'], f'{res.json()["data"]["slug"]}.tar')
@@ -31,10 +55,15 @@ def create_and_upload_backup():
         log('ERROR | Backup creation failed!')
         raise Exception(res.text)
 
-cron = croniter.croniter(hass_options['cron'], datetime.now())
-next_backup = cron.get_next(datetime)
+encrypt_with_pgp()
 while True:
-    if datetime.now() >= next_backup:
-        create_and_upload_backup()
-        next_backup = cron.get_next(datetime)
-    time.sleep(10)
+    time.sleep(1)
+
+
+#cron = croniter.croniter(hass_options['cron'], datetime.now())
+#next_backup = cron.get_next(datetime)
+#while True:
+#    if datetime.now() >= next_backup:
+#        create_and_upload_backup()
+#        next_backup = cron.get_next(datetime)
+#    time.sleep(10)
